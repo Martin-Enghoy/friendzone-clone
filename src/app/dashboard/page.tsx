35e7d871ae2782b103dba81bc21f8a52118f58be
Martin-Enghoy <adrianenghoy@gmail.com>
2023-10-31 14:57:58 +0800

    dev: initialized redis db in app

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/ui/Button";
+import { FunctionComponent } from "react";
+
+interface DashboardPageProps {}
+
+const DashboardPage: FunctionComponent<DashboardPageProps> = () => {
+  return (
+    <Button size="default" variant="default">
+      Hello
+    </Button>
+  );
+};
+
+export default DashboardPage;
